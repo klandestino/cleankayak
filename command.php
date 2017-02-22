f2abf76fb676be74cb35afee7a9ff3db95dee1a4
redundans <jesper@klandestino.se>
2017-02-22 10:32:24 +0100

    dry run

--- command.php
+++ command.php
@@ -12,8 +12,13 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 $clean_kayak_command = function() {
 	$response = WP_CLI::launch_self( 'user list --role=subscriber', array(), array( 'format' => 'json' ), false, true );
 	$users = json_decode( $response->stdout );
+
+	$kayak = new Kayak_Subscriber_Manager();
+
 	foreach ( $users as $user ) {
 		WP_CLI::log( "Användare med mail: {$user->user_email}" );
+		$isactive = $kayak->email_has_active_subscription( $user->user_email );
+		WP_CLI::log( "Är aktiv: $isactive}" );
 	}
 };
 WP_CLI::add_command( 'clean-kayak', $clean_kayak_command );
