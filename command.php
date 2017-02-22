fbaccee151f28b7fd623204c0803e0f38acd4d56
redundans <jesper@klandestino.se>
2017-02-22 10:58:48 +0100

    dry run

--- command.php
+++ command.php
@@ -19,7 +19,7 @@ $clean_kayak_command = function() {
 		if ( $isactive ) {
 			WP_CLI::log( "Är aktiv!" );
 		} else {
-			$response = WP_CLI::launch_self( 'user delete '.$user->ID, array(), array( 'format' => 'json' ), false, true );
+			$response = WP_CLI::runcommand( 'user delete '.$user->ID, array() );
 			var_dump($response);
 			//WP_CLI::log( "Tar bort inaktiv!" );
 		}
