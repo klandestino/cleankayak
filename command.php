f807ed6eef88b04fa112fbbae1137070b41ebb11
redundans <jesper@klandestino.se>
2017-02-22 10:37:38 +0100

    dry run

--- command.php
+++ command.php
@@ -13,12 +13,33 @@ $clean_kayak_command = function() {
 	$response = WP_CLI::launch_self( 'user list --role=subscriber', array(), array( 'format' => 'json' ), false, true );
 	$users = json_decode( $response->stdout );
 
-	$kayak = new Kayak_Subscriber_Manager();
-
 	foreach ( $users as $user ) {
 		WP_CLI::log( "Användare med mail: {$user->user_email}" );
-		$isactive = $kayak->email_has_active_subscription( $user->user_email );
+		email_has_active_subscription( $user->user_email );
 		WP_CLI::log( "Är aktiv: $isactive}" );
 	}
 };
+
+function email_has_active_subscription( $email ) {
+	try {
+		$client = new SoapClient('http://91.209.29.10:8080/KayakWebServiceARB/KayakWebService.asmx?WSDL', array('trace' => 1));
+		$args = array(
+			'sEmail' => $email
+		);
+		$response = $client->GetCusByEmail($args);
+		
+		$xml = simplexml_load_string($response->GetCusByEmailResult->any);
+		if($xml->NewDataSet->CUSTOMER){
+			foreach ( $xml->NewDataSet->CUSTOMER as $customer){
+				if ( Kayak_Subscriber_Manager::has_active_subscription( $customer->CUSNO ) ) {
+					return true;
+				}
+			}
+		}
+	} catch (SoapFault $soapFault) {
+		var_dump($soapFault);
+	}
+	return false;
+}
+
 WP_CLI::add_command( 'clean-kayak', $clean_kayak_command );
